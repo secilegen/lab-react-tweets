82da7fefd8125c83c77db07848cc087d15b45fe6
Secil <secilegen@gmail.com>
2023-02-16 21:44:50 +0100

    lab done

---
+++ src/components/User.js
@@ -0,0 +1,10 @@
+function User(props) {
+    return(
+        <span className="user">
+            <span className="name">{props.userdata.name}</span>
+            <span className="handle">@{props.userdata.handle}</span>
+          </span>
+    )
+}
+
+export default User
